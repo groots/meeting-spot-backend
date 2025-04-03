c6f93e0e84282590290f355655bfb9b0a0d27643
George Roots <squish.roots@gmail.com>
2025-04-03 17:47:28 -0400

    updating to try to fix github deploy

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="find_a_meeting_spot",
     version="0.1.0",
-    packages=find_packages(include=["app", "app.*", "tests", "tests.*"]),
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "Flask==2.0.1",
         "Flask-SQLAlchemy==2.5.1",
