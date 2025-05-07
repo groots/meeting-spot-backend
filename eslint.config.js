cd2080eb98f6cbc3f6a018591e4d363bec47b474
George Roots <squish.roots@gmail.com>
2025-05-07 10:44:37 -0400

    Fix pre-commit hooks and add ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    ignores: ['dist/**', 'node_modules/**'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'no-console': 'warn',
+    },
+  }
+); 
